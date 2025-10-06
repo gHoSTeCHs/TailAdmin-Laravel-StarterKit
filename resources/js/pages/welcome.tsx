39f488e9e871f1360b9e82474984eafb45c3afa0
Somadina <kingswatter@gmail.com>
2025-10-06 10:25:46 +0100

    view now working

---
+++ resources/js/pages/welcome.tsx
@@ -0,0 +1,8 @@
+const WelcomePage = () => {
+    return (<div>
+
+        <p>This is the new welcome page</p>
+    </div>)
+}
+
+export default WelcomePage
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/welcome', function () {
+    return Inertia::render('welcome', []);
 });
